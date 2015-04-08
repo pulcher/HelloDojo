ce22e5a2c8a23d80033eee55726d42a0091028ec
Michael L Perry <michael@qedcode.com>
2015-04-08 07:24:20 -0500

    Added Lister application.

---
+++ Android/Lister/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Assisticant.Binding;
+
+namespace Lister
+{
+	[Activity (Label = "Lister", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		private BindingManager _bindings = new BindingManager();
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			_bindings.Initialize (this);
+		}
+
+		protected override void OnDestroy()
+		{
+			base.OnDestroy();
+
+			_bindings.Unbind();
+		}
+	}
+}
+
+
---
+++ iOS/Lister/ListerViewController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using Assisticant.Binding;
+
+namespace Lister
+{
+	public partial class ListerViewController : UIViewController
+	{
+		private BindingManager _bindings = new BindingManager();
+
+		public ListerViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+			_bindings.Initialize (this);
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+	}
+}
+
