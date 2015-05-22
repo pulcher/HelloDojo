b6ea8c55974202c7d6233135d7c19a6f6708bd09
Michael L Perry <michael@qedcode.com>
2015-05-21 23:08:46 -0500

    Bind the master view controller to the main view model.

--- Portable/WeatherApp.iOS/MasterViewController.cs
+++ Portable/WeatherApp.iOS/MasterViewController.cs
@@ -5,12 +5,15 @@ using System.Collections.Generic;
 using CoreGraphics;
 using Foundation;
 using UIKit;
+using WeatherApp.Logic.ViewModels;
+using Assisticant.Binding;
 
 namespace WeatherApp
 {
     public partial class MasterViewController : UITableViewController
     {
-        DataSource dataSource;
+        private MainViewModel _viewModel;
+        private BindingManager _bindings = new BindingManager();
 
         partial void InitializeViewModelLocator();
 
@@ -18,6 +21,7 @@ namespace WeatherApp
             : base(handle)
         {
             InitializeViewModelLocator();
+            _viewModel = ViewModelLocator.Instance.Main;
 
             Title = NSBundle.MainBundle.LocalizedString("Master", "Master");
 			
@@ -38,10 +42,7 @@ namespace WeatherApp
 
         void AddNewItem(object sender, EventArgs args)
         {
-            dataSource.Objects.Insert(0, DateTime.Now);
-
-            using (var indexPath = NSIndexPath.FromRowSection(0, 0))
-                TableView.InsertRows(new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Automatic);
+            PerformSegue("newCity", this);
         }
 
         public override void DidReceiveMemoryWarning()
@@ -62,98 +63,48 @@ namespace WeatherApp
             var addButton = new UIBarButtonItem(UIBarButtonSystemItem.Add, AddNewItem);
             NavigationItem.RightBarButtonItem = addButton;
 
-            TableView.Source = dataSource = new DataSource(this);
+            _bindings.Initialize(this);
         }
 
-        class DataSource : UITableViewSource
+        public override void ViewWillAppear(bool animated)
         {
-            static readonly NSString CellIdentifier = new NSString("Cell");
-            readonly List<object> objects = new List<object>();
-            readonly MasterViewController controller;
+            base.ViewWillAppear(animated);
 
-            public DataSource(MasterViewController controller)
-            {
-                this.controller = controller;
-            }
+            _viewModel.SelectedCityHeader = null;
 
-            public IList<object> Objects
-            {
-                get { return objects; }
-            }
-
-            // Customize the number of sections in the table view.
-            public override nint NumberOfSections(UITableView tableView)
-            {
-                return 1;
-            }
-
-            public override nint RowsInSection(UITableView tableview, nint section)
-            {
-                return objects.Count;
-            }
-
-            // Customize the appearance of table view cells.
-            public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
-            {
-                var cell = (UITableViewCell)tableView.DequeueReusableCell(CellIdentifier, indexPath);
-
-                cell.TextLabel.Text = objects[indexPath.Row].ToString();
-
-                return cell;
-            }
-
-            public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath)
-            {
-                // Return false if you do not want the specified item to be editable.
-                return true;
-            }
-
-            public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, NSIndexPath indexPath)
-            {
-                if (editingStyle == UITableViewCellEditingStyle.Delete)
+            _bindings.BindItems(
+                TableView,
+                () => _viewModel.CityHeaders,
+                () => _viewModel.SelectedCityHeader,
+                value => _viewModel.SelectedCityHeader = value,
+                (cell, city, bindings) =>
                 {
-                    // Delete the row from the data source.
-                    objects.RemoveAt(indexPath.Row);
-                    controller.TableView.DeleteRows(new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Fade);
-                }
-                else if (editingStyle == UITableViewCellEditingStyle.Insert)
-                {
-                    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-                }
-            }
+                    bindings.BindText(
+                        cell.TextLabel,
+                        () => city.Name);
+                });
 
-            /*
-			// Override to support rearranging the table view.
-			public override void MoveRow (UITableView tableView, NSIndexPath sourceIndexPath, NSIndexPath destinationIndexPath)
-			{
-			}
-			*/
-
-            /*
-			// Override to support conditional rearranging of the table view.
-			public override bool CanMoveRow (UITableView tableView, NSIndexPath indexPath)
-			{
-				// Return false if you do not want the item to be re-orderable.
-				return true;
-			}
-			*/
-
-            public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
-            {
-                if (UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Pad)
-                    controller.DetailViewController.SetDetailItem(objects[indexPath.Row]);
-            }
+            _bindings.Bind(
+                () => _viewModel.SelectedCityHeader,
+                cityHeader => OnSelected(cityHeader));
         }
 
-        public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
+        public override void ViewDidDisappear(bool animated)
         {
-            if (segue.Identifier == "showDetail")
-            {
-                var indexPath = TableView.IndexPathForSelectedRow;
-                var item = dataSource.Objects[indexPath.Row];
+            _bindings.Unbind();
 
-                ((DetailViewController)segue.DestinationViewController).SetDetailItem(item);
-            }
+            base.ViewDidDisappear(animated);
+        }
+
+        [Action("UnwindToMaster:")]
+        public void UnwindToMaster(UIStoryboardSegue segue)
+        {
+        }
+
+        private void OnSelected(CityHeaderViewModel cityHeader)
+        {
+            if (cityHeader != null)
+                PerformSegue("showDetail", this);
         }
     }
 }
